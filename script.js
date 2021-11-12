3f971f460aa3e7bb080a7366b9559e26dfdc8c2a
Owusu-Desmond <desmond0406114009@gmail.com>
2021-11-12 08:08:05 +0000

    website with responsive grid system and javascript

---
+++ script.js
@@ -0,0 +1,15 @@
+function btnWithMenuIcon(){
+    document.getElementById('btnWithMenuIcon').style.display="none";
+    document.getElementById('btnWithCloseIcon').style.display="block";
+    document.getElementById('menuItems').style.display="grid";
+
+
+    }
+       
+function btnWithCloseIcon(){
+    document.getElementById('btnWithCloseIcon').style.display="none";
+    document.getElementById('btnWithMenuIcon').style.display="block";
+    document.getElementById('menuItems').style.display="none";
+
+
+}
